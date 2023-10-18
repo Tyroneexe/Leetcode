11cd7ff395332b1edec94db88113ddd335553db5
Tyroneexe <tvzbothma@gmail.com>
2023-10-18 20:23:35 +0200

    valid parentheses leetcode question

---
+++ valid_parentheses.py
@@ -0,0 +1,22 @@
+class Solution:
+    def isValid(self, s: str) -> bool:
+        stack = []
+
+    
+        bracket_pairs = {')': '(', '}': '{', ']': '['}
+
+        for char in s:
+            if char in '({[':
+                stack.append(char)
+            elif char in ')}]':
+                if not stack or stack.pop() != bracket_pairs[char]:
+                    return False 
+        return not stack 
+
+                
+      
+    
+solution = Solution()
+string = "()"
+
+print(solution.isValid(string))
